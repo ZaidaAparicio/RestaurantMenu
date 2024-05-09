de1fb78bf0bbf37502af0157838e0f9517b8f963
Zaida Aparicio <zaida.aparicio@diimo.co>
2024-05-09 15:47:50 -0600

    🎉

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app">
+        <div id="main-container">
+          <!--  -->
+            <MyHome></MyHome>
+        </div>
+    </div>
+</template>
+
+<script>
+    import MyHome from './components/MyHome.vue';
+
+    export default {
+        name: 'App',
+        components: {
+            MyHome,
+        }
+    }
+</script>
+
---
+++ src/components/MyHome.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="my-home">
+        <h1>Componente Nuevo</h1>
+        <h3>{{ menu }}</h3>
+    </div>
+</template> 
+
+<script>
+    export default {
+        name: 'MyHome',
+        data() {
+            return {
+                menu: 'prueba 1'
+            }
+        }
+    }
+</script>
